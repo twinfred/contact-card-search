b76f573f062e2c48be820bc2fcade38fb3a96e09
Tim Winfred <twinfred@gmail.com>
2021-04-23 23:04:44 -0700

    Updated card component to pull in cat images from robohash for user avatar

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.css';
 
 export const Card = props => (
   <div className="card-container">
-    <img src={props.contact.avatar} alt=""/>
+    <img src={`https://robohash.org/${props.contact.id}?set=set4&size=180x180`} alt=""/>
     <h2>{props.contact.first_name} {props.contact.last_name}</h2>
     <p>{props.contact.email}</p>
   </div>
